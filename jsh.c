5f0921c8f45ad8ef3b4d3863ea65edad7015c27b
jonathan0 <jonathan03ant@gmail.com>
2024-06-16 01:08:48 +0100

    Update, Plan #1

--- jsh.c
+++ jsh.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+
+
+
+
+
+
+
+
+int main (int argc, char **argv){
+	//we need to load any config files (if any)
+	// interpret commands
+		// Read the command, 
+		// Parse he command, separate command stirings in to argument
+		// Excute the command
+	// Terminate 
