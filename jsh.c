19b5a5d62e77d9ce6a6a585c9ee580addf9c5f1e
jonathan0 <jonathan03ant@gmail.com>
2024-06-16 19:12:22 +0100

    Update jsh_lo() #1

--- jsh.c
+++ jsh.c
@@ -13,4 +13,29 @@ int main (int argc, char **argv){
 		// Read the command, 
 		// Parse he command, separate command stirings in to argument
 		// Excute the command
-	// Terminate 
+	// Terminate
+	
+	jsh_lop();
+
+
+	return EXIT_SUCCESS;
+}
+
+
+void jsh_lo(void){
+	//read line, pharse the commmands, then execute the command
+
+	char *line;
+	char **args;
+	int status;
+	
+	do {
+		printf(">");
+		line = ReadLine();
+		args = PharseLine();
+		status = Execute();
+
+	} while(status);
+
+}
+
